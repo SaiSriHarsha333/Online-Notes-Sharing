543c61ddd35981c3a73476aa1bb304fda637254a
srisumanth999 <ysrisumanth@gmail.com>
2020-05-23 13:57:07 +0530

    Default users and uploads added in database

--- src/db/notes.sql
+++ src/db/notes.sql
@@ -31,6 +31,11 @@ CREATE TABLE `uploads` (
   `status` varchar(225) NOT NULL DEFAULT 'not approved yet'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+INSERT INTO `uploads` (`file_id`, `file_name`, `file_description`, `file_type`, `file_uploader`, `file_uploaded_on`, `file_uploaded_to`, `file`, `status`) VALUES
+(61, 'DEVOPS Project Report', 'Handwriting recognition', 'pdf', 'srikar', '2020-05-23 09:49:51', 'Electrical', '866298.pdf', 'approved'),
+(62, 'SPE', 'goods availability management', 'pdf', 'harsha', ' 2020-05-23 11:03:47', 'Electrical', '981571.pdf', 'approved'),
+(60, 'E Commerce Website', 'e commerce website in devops', 'pdf', 'manideep', ' 2020-05-23 09:34:23', 'Computer Science', '804917.pdf', 'approved'),
+(63, 'SPE Project Report', 'auto attendance management', 'pdf', 'harsha', '2020-05-23 11:05:10', 'Electrical', '38391.pdf', 'approved');
 
 
 -- --------------------------------------------------------
@@ -59,7 +64,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `name`, `about`, `role`, `email`, `token`, `gender`, `password`, `course`, `image`, `joindate`) VALUES
-(1, 'admin', 'admin', 'N/A', 'admin', 'root@gmail.com', '', 'N/A', '$2y$10$UExd.f8vQXogrZELXF8KGulQJKUn32p8x4B5SVQ7V/D6.mrSAkAjW', 'Computer Science', 'profile.jpg', '2000-01-01');
+(1, 'admin', 'admin', 'N/A', 'admin', 'root@gmail.com', '', 'N/A', '$2y$10$UExd.f8vQXogrZELXF8KGulQJKUn32p8x4B5SVQ7V/D6.mrSAkAjW', 'Computer Science', 'profile.jpg', '2000-01-01'),
+(29, 'sumanth', 'sumanth', 'N/A', 'student', 'sumanth@gmail.com', '', 'Male', '$2y$10$xEEu0hAtrJWCh3klz4DBF.5a4EMFsWaIAsFT7Jt2RBVgMVrRxz3Li', 'Computer Science', 'profile.jpg', 'May 23, 2020'),
+(31, 'manideep', 'manideep', 'N/A', 'student', 'manideep@gmail.com', '', 'Male', '$2y$10$JYLVNL/IHWLJBFk8Chkfg.umjWu1Zbymy6piqja2lvZv3wxktmqRy', 'Computer Science', 'profile.jpg', 'May 23, 2020'),
+(32, 'harsha', 'harsha', 'N/A', 'student', 'harsha@gmail.com', '', 'Male', '$2y$10$4JK2uRNN8FHrY/Zs96AmvurQpMP8p409ShvQ9zGAYOTAeAd7ppozS', 'Electrical', '771547.jpg', 'May 23, 2020'),
+(34, 'srikar', 'srikar', 'N/A', 'student', 'srikar@gmail.com', '', 'Male', '$2y$10$nld.q3PLHGktG3/Q.xCpheGRZ5omPXJpWlgHgU5CANJPSajp8i//a', 'Electrical', '421040.jpg', 'May 23, 2020');
 --
 -- Indexes for dumped tables
 --
