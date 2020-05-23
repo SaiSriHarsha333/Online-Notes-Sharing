b2aa9ebb22cfbeda81bcdacf1da51c92041a61cf
srisumanth999 <ysrisumanth@gmail.com>
2020-05-23 14:08:30 +0530

    small CSS changes

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -163,15 +163,15 @@ while ($row2 = mysqli_fetch_array($run_query2)) {
 $variable = substr($file, 0, strpos($file, '.'));
 
 
-    echo '<div class="w3-container w3-cell w3-half w3-border w3-margin-bottom w3-margin-top w3-border-blue w3-round-large"><br>';
+    echo '<div class="w3-container w3-cell w3-half w3-border w3-margin-bottom w3-margin-top w3-border-blue w3-round-large" style="box-shadow: -10px 10px 5px grey;"><br>';
 
-    echo '<img src="profilepics/' . $profile . '" alt="Avatar" class="w3-left w3-circle w3-margin-right w3-border w3-border-green" style="width:60px">';
+    echo '<img src="profilepics/' . $profile . '" alt="Avatar" class="w3-left w3-circle w3-margin-right w3-border w3-border-green" style="width:60px; box-shadow: 5px 5px 5px grey;">';
 
-    echo '<a href = "viewprofile.php?name=' . $file_uploader . '", style="text-decoration:none"><h4>' . $file_uploader . '</h4><br></a>';
+    echo '<a href = "viewprofile.php?name=' . $file_uploader . '", style="text-decoration:none"><h4 style="  text-shadow: 2px 2px 4px grey;">' . $file_uploader . '</h4><br></a>';
     echo '<span class="w3-right w3-opacity w3-border w3-border-brown"> ' . $file_date .  '</span>';
 
-    echo '<h3 style="font-size:20px;"> Title : ' . $file_name . ' </h3>';
-    echo '<h3 class="w3-border w3-pale-red" style="font-size:20px;"> Description : ' . $file_description . ' </h3>';
+    echo '<h3 style="font-size:20px; text-shadow: 1px 1px 2px grey;"> Title : ' . $file_name . ' </h3>';
+    echo '<h3 class="w3-border w3-white" style="font-size:20px; text-shadow: 1px 1px 2px grey;"> Description : ' . $file_description . ' </h3>';
     echo "<a href='allfiles/" . $file . "' target='_blank' style='color:green'>Download </a>";
     echo '<hr class="w3-clear">';
 
